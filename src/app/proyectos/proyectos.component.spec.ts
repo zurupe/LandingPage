4a6e71325b03410d066d6965f17ae5c1b7e92c66
Pablo Zurita <pablozurita.jr@outlook.com>
2026-06-03 23:28:02 +0000

    Bug fixed with the deploy

--- src/app/proyectos/proyectos.component.spec.ts
+++ src/app/proyectos/proyectos.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Proyectos } from './proyectos.component';
 
@@ -8,7 +9,7 @@ describe('Proyectos', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Proyectos]
+      imports: [Proyectos, RouterTestingModule]
     })
     .compileComponents();
 
